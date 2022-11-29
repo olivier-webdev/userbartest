6f630a7ab5b4586da5be984c1bec0634f1d16a8f
olivier-webdev <olivier.webdev@gmail.com>
2022-11-29 16:43:16 +0100

    test 34

--- src/data/beers.js
+++ src/data/beers.js
@@ -9,7 +9,7 @@ export const dataBeers = [
   },
   {
     id: 2,
-    name: "Triple Karmeliet 3K Test",
+    name: "Triple Karmeliet 3K Test le roi de la flechette",
     price: 6.5,
     image: "assets/images/tripleK.jpg",
     description:
